35a10bcecad286a7b202d1b953b6c0ea50a87afc
brisacazares17 <79296074+brisacazares17@users.noreply.github.com>
2022-05-14 12:11:17 -0700

    Carrusel
    Integradora Brisa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const items = [
+  {
+    src: require('./img/1.jpg'),
+    altText: 'Slide 1', 
+  },
+  {
+    src: require('./img/2.jpg'),
+    altText: 'Slide 2',
+  },
+  {
+    src: require('./img/3.jpg'),
+    altText: 'Slide 3',
+  },
+  {
+    src: require('./img/4.jpg'),
+    altText: 'Slide 2',
+  },
+  {
+    src: require('./img/5.jpg'),
+    altText: 'Slide 2', 
+  },
+  {
+    src: require('./img/6.jpg'),
+    altText: 'Slide 2',
+  },
+  {
+    src: require('./img/7.jpg'),
+    altText: 'Slide 2',
+  },
+  {
+    src: require('./img/8.jpg'),
+    altText: 'Slide 2',
+  },
+  {
+    src: require('./img/9.jpg'),
+    altText: 'Slide 2',
+  },
+  {
+    src: require('./img/10.jpg'),
+    altText: 'Slide 2',
+  },
+  {
+    src: require('./img/11.jpg'),
+    altText: 'Slide 2',
+  },
+  {
+    src: require('./img/12.jpg'),
+    altText: 'Slide 2',
+  }
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <img src={item.src} alt={item.altText} width="100%" height="650px" />
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
+    );
+  }
 }
+
 
 export default App;
